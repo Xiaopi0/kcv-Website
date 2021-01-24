ff0ae91c5530753f6a6b3a4189000296505121e5
Xiaopi0 Studios <erikkirkegaardstage@gmail.com>
2021-01-24 12:41:51 +0100

    Create Basic Structure And Move Files

---
+++ old/msgs/sender.php
@@ -0,0 +1,30 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: ../index.php");
+    exit;
+}
+
+$file = 'msgs.xml';
+
+$xml = simplexml_load_file($file) or die("Error: Cannot create object");
+
+$msgs = $xml->msgs;
+$info = $xml->info;
+$id = $info->id + 1;
+
+$msg = $msgs->addChild('msg');
+$msg->addChild('id', $id);
+$msg->addChild('to', $_GET["to"]);
+$msg->addChild('from', $_SESSION["username"]);
+$msg->addChild('desc', $_GET["desc"]);
+$msg->addChild('body', $_GET["body"]);
+$xml->info->id = $id;
+
+$xml->asXML($file);
+
+header("location: ../msg.php?t=out")
+ ?>
---
+++ old/msgs/sendmsg.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Send Besked</title>
+  </head>
+  <body>
+    <h1>WIP</h1>
+     <form action="msgs/sender.php" method="get">
+       Til: <input type="text" name="to"><br>
+       Emne: <input type="text" name="desc"><br>
+       <p>Besked: </p>
+       <textarea id="body" name="body" style="margin: 1px 85px 0px 0px; width: 402px; height: 284px;"></textarea><br>
+       <input type="submit">
+     </form>
+  </body>
+</html>
